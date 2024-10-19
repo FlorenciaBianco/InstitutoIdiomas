a0e5d08228898f6d4d5672254fb1cd1217a979fd
Mateo Biondi <mateobiondi05@gmail.com>
2024-10-18 22:54:57 -0300

    Revert "deploy arreglado vol2"
    This reverts commit 827ee8ed052bd11f55f6438289175bef14c9ba90.

--- app/models/profesor.model.php
+++ app/models/profesor.model.php
@@ -1,11 +1,12 @@
 <?php
 require_once 'config.php';
-
+require_once 'app/models/deploy.model.php';
 class ProfesorModel {
     private $db;
 
     public function __construct (){
         $this->db = new PDO("mysql:host=".MYSQL_HOST .";dbname=".MYSQL_DB.";charset=utf8", MYSQL_USER, MYSQL_PASS);
+        $this->deploy();
     }
     
     public function getAll() {
